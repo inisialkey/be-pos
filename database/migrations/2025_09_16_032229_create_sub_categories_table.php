5aa352840f7cca45c1cbf33a2474a04d7335c8f3
inisialkey <okidorvel404@gmail.com>
2025-09-16 10:33:14 +0700

    feat(migration): add table users, categories, sub_catogories, products, orders

--- database/migrations/2025_09_16_032229_create_sub_categories_table.php
+++ database/migrations/2025_09_16_032229_create_sub_categories_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sub_categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            // $table->string('roles')->default('user');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sub_categories');
     }
 };
